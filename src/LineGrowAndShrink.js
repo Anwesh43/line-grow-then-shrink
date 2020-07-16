097c06f52dbfc3b6cb1b24e6e21807de84367c7f
Anwesh43 <anweshthecool0@gmail.com>
2020-07-16 16:58:50 +0530

    adding LineGrowAndShrinkContainer

--- src/LineGrowAndShrink.js
+++ src/LineGrowAndShrink.js
@@ -3,7 +3,7 @@ import {useStyle} from './hooks'
 
 const LineGrowAndShirnk = ({w, h, scale, onClick}) => {
     const {getLineStyle} = useStyle(w, h, scale)
-    return <div>
+    return <div onClick = {onClick}>
         <div style = {getLineStyle()}>
         </div>
     </div>
---
+++ src/LineGrowAndShrinkContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useAnimatedScale, useDimension} from './hooks'
+import LineAndGrowShrink from './LineGrowAndShrink'
+
+const LineAndGrowShrinkContainer = ({props}) => {
+    const {scale, start} = useAnimatedScale(0.01, 20)
+    const {w, h} = useDimension()
+    return <div>
+        <LineAndGrowShrink w = {w} h = {h} scale = {scale} onClick = {start}></LineAndGrowShrink>
+    </div>
+}
+export default LineAndGrowShrinkContainer
